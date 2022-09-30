b6ab856d10be8df46aa073eac6ac2eef1321100f
ypprog <pan.yiping.fi@gmail.com>
2022-09-30 17:24:05 +0800

    Add 86_Partition_List

---
+++ 86_Partition_List.cpp
@@ -0,0 +1,47 @@
+tion for singly-linked list.
+ * struct ListNode {
+  *int val;
+  *ListNode *next;
+  *ListNode()
+      : val(0),
+  next(nullptr){} * ListNode(int x)
+      : val(x),
+  next(nullptr){} * ListNode(int x, ListNode *next) : val(x), next(next){} *
+};
+* / class Solution {
+public:
+  ListNode *partition(ListNode *head, int x) {
+    if (!head)
+      return head;
+
+    ListNode *dummy = new ListNode(0);
+    dummy->next = head;
+
+    ListNode *tail_lt = dummy;
+    ListNode *prev = dummy;
+    ListNode *curr = head;
+    ListNode *peek;
+
+    while (curr && curr->val < x) {
+      curr = curr->next;
+      prev = prev->next;
+    }
+    tail_lt = prev;
+
+    while (curr) {
+      peek = curr->next;
+
+      if (curr->val < x) {
+        curr->next = tail_lt->next;
+        tail_lt->next = curr;
+        tail_lt = tail_lt->next;
+        prev->next = peek;
+        curr = peek;
+      } else {
+        prev = curr;
+        curr = peek;
+      }
+    }
+    return dummy->next;
+  }
+};
