f5e728ab68155db771b9c47811f8192e3c049edd
ypprog <pan.yiping.fi@gmail.com>
2022-10-03 20:17:28 +0800

    Add 083

---
+++ Linked_List/083_Remove_Duplicates_from_Sorted_List.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *deleteDuplicates(ListNode *head) {
+
+    ListNode *it = head;
+    while (it && it->next) {
+        if (it->val == it->next->val) {
+            it->next = it->next->next;
+        } else {
+            it = it->next;
+        }
+    }
+    return head;
+  }
+};
