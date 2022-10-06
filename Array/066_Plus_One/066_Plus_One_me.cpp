3c42cdca33e9bb64ae35fbd5f3a6895df142f1e5
ypprog <pan.yiping.fi@gmail.com>
2022-10-06 19:13:37 +0800

    Modify 066_Plus_One

--- Array/066_Plus_One/066_Plus_One_me.cpp
+++ Array/066_Plus_One/066_Plus_One_me.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+        int len = digits.size();
         
-    int carry = 0;
-    for (auto it = digits.end(); i != degits.begin(); i--) {
-       *i = *i + 1 
+        ++digits[len-1];
+        if (digits[len-1] < 10) {
+            return digits;
+        }
+        int carry = 0;
+        for (int i=len-1; i>=0; i--) {
+            int sum = carry + digits[i];
+            carry = sum / 10;
+            digits[i] = sum % 10;
+            if (carry == 0) return digits;
+        }
+        if (carry == 1) {
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
     }
-}
-
+};
