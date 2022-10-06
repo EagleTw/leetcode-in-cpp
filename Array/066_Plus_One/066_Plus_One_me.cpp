96883e663e4460c27006dbab39f89a08013818c4
ypprog <pan.yiping.fi@gmail.com>
2022-10-06 19:10:45 +0800

    Add 066

---
+++ Array/066_Plus_One/066_Plus_One_me.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        
+    int carry = 0;
+    for (auto it = digits.end(); i != degits.begin(); i--) {
+       *i = *i + 1 
+    }
+}
+
