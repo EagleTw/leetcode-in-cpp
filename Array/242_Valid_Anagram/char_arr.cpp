1a5e3612693e12d21a587a3c4d3c1830f4ddd5cf
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2023-06-15 20:41:10 +0800

    Add 242 Valid Anagram

---
+++ Array/242_Valid_Anagram/char_arr.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+  bool isAnagram(string s, string t) {
+    if (s.size() != t.size())
+        return false;
+
+    int alphabet[26] = {0};
+
+    for (int i = 0; i < s.size(); i++) {
+      alphabet[s[i] - 'a']++;
+    }
+
+    for (int i = 0; i < t.size(); i++) {
+      if(--alphabet[t[i] - 'a'] < 0)
+        return false;
+    }
+    return true;
+  }
+};
---
+++ Array/242_Valid_Anagram/map.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+  bool isAnagram(string s, string t) {
+
+    if (s.size() != t.size())
+      return false;
+
+    std::map<char, int> charMap;
+
+    for (int i = 0; i < s.size(); i++) {
+      charMap[s[i]]++;
+    }
+    for (int i = 0; i < t.size(); i++) {
+      charMap[t[i]]--;
+      if (charMap[t[i]] < 0)
+        return false;
+    }
+    return true;
+  }
+};
