3d098498a1bc3b0b9a9be13f30348057689a9476
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2023-06-18 23:07:35 +0800

    Add 020 Valid Parentheses

---
+++ Stack/020_Valid_Parentheses/switch.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::stack<char> paraStack;
+
+        for (auto c : s) {
+            char expected;
+            switch(c) {
+            case '{':
+            case '(':
+            case '[':
+                paraStack.push(c);
+                break;
+            case '}':
+                if (paraStack.empty() || paraStack.top() != '{')
+                    return false;
+                paraStack.pop();
+                break;
+            case ')':
+                if (paraStack.empty() || paraStack.top() != '(')
+                    return false;
+                paraStack.pop();
+                break;
+            case ']':
+                if (paraStack.empty() || paraStack.top() != '[')
+                    return false;
+                paraStack.pop();
+                break;
+            }
+        }
+
+        return paraStack.empty();
+    }
+};
