f6212cdf4cb294880e68b086adaa6d2849d7b48c
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2023-06-20 08:41:24 +0800

    Add 167. Two Sum II - Input Array Is Sorted

---
+++ Two-Pointers/167_TwoSumII_Input_Array_Is_Sorted/two_sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ret;
+        int front = 0;
+        int rear = numbers.size() - 1;
+
+        while (front < rear) {
+            int sum = numbers[front] + numbers[rear];
+            if (sum == target) {  // matches
+                ret.push_back(front + 1);
+                ret.push_back(rear + 1);
+                break;
+            } else if (sum > target) {
+                rear--;
+            } else {
+                front++;
+            }
+        }
+        return ret;
+    }
+};
