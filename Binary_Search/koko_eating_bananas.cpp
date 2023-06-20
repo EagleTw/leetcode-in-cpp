d8eb4fd8b87f488b2124245d36c9bfd11fd5b289
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2023-06-20 21:31:42 +0800

    Add 875. Koko Eating Bananas

---
+++ Binary_Search/koko_eating_bananas.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    double calcH(vector<int> &piles, int div) {
+        if (div == 0)
+            return INT_MAX;
+
+        double ret = 0;
+        for (int i : piles) {
+            ret += (i % div) ? (i / div) + 1 : (i / div);
+        }
+        return ret;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+    
+        int low = 0;  // lower bound of k
+        int high = 0;
+        
+        for (int i : piles) {
+            high = max(high, i);
+        } 
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+            double val = calcH(piles, mid);
+            printf("Low: %d, High:%d, val:%d\n", low, high, val);
+            if (val > h) { /* val too large */
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+};
