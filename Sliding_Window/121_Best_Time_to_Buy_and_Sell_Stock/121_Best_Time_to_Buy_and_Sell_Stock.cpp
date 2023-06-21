ba7b4c30857164a9022e0e428aabd647f3c8557d
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2023-06-21 16:24:51 +0800

    Add 121. Best Time to Buy and Sell Stock

---
+++ Sliding_Window/121_Best_Time_to_Buy_and_Sell_Stock/121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        // brute force
+        size_t vec_size = prices.size();
+        int max_profit = INT_MIN;
+        int l = 0, r = 0;
+
+        for (; r < vec_size; r++) {
+            if (prices[r] < prices[l]) {
+               l = r;
+            }
+            max_profit = max(max_profit, prices[r] - prices[l]);
+        }
+        return max_profit;
+    }
+};
+
