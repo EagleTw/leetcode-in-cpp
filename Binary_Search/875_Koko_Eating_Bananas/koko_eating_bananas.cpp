7a19028541d9aa8af709098bb4e792b27619a648
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2023-06-21 19:50:14 +0800

    Move files

--- Binary_Search/875_Koko_Eating_Bananas/koko_eating_bananas.cpp
+++ Binary_Search/875_Koko_Eating_Bananas/koko_eating_bananas.cpp
@@ -12,18 +12,17 @@ public:
     }
 
     int minEatingSpeed(vector<int>& piles, int h) {
-    
+
         int low = 0;  // lower bound of k
         int high = 0;
-        
+
         for (int i : piles) {
             high = max(high, i);
-        } 
+        }
 
         while (low < high) {
             int mid = (low + high) / 2;
             double val = calcH(piles, mid);
-            printf("Low: %d, High:%d, val:%d\n", low, high, val);
             if (val > h) { /* val too large */
                 low = mid + 1;
             } else {
