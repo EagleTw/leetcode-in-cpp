ff6984feae9dd9fe190f2779d34ed3ea62652ad0
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2023-07-01 14:07:58 +0800

    Add better solution

--- Linked_List/206_Reverse_Linked_List/206_Reverse_Linked_List.cpp
+++ Linked_List/206_Reverse_Linked_List/206_Reverse_Linked_List.cpp
@@ -1,29 +1,20 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
  public:
-  ListNode *reverseList(ListNode *head) {
+  ListNode* reverseList(ListNode* head) {
     if (head == nullptr) {
       return NULL;
     }
+    ListNode* prev = nullptr;
+    ListNode* curr = head;
 
-    ListNode *prev = nullptr, *lookahead = head->next;
-
-    while (lookahead != nullptr) {
-      head->next = prev;
-      prev = head;
-      head = lookahead;
-      lookahead = head->next;
+    while (curr) {
+      ListNode* n = curr->next;
+      curr->next = prev;
+      prev = curr;
+      curr = n;
     }
-    head->next = prev;
+
+    head = prev;
 
     return head;
   }
