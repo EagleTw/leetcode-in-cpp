31b1ff36a0610383b51268e8c671c36dd1ee98b2
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2023-07-03 22:06:23 +0800

    Add 070_Climbing_Stairs

---
+++ Dynamic_Programming/1D/070_Climbing_Stairs/070_Climbing_Stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+  int climbStairs(int n) {
+    int dp[n + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+
+    if (n < 2) return dp[n];
+
+    for (int i = 2; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+  }
+};
